906331b7a6a97f59be67162f21b90a163988ee66
Iced Mist <talk2icedmist@gmail.com>
2025-07-06 14:19:06 +0000

    dqrHnJtM27bMpNudHxO5hL3wsNE3

--- src/lib/admin.ts
+++ src/lib/admin.ts
@@ -1,3 +1,3 @@
 // This file contains the list of Firebase UIDs for users who should have admin privileges.
 // IMPORTANT: Replace the placeholder value with your actual Firebase User UID.
-export const ADMIN_UIDS = ['REPLACE_WITH_YOUR_ADMIN_UID'];
+export const ADMIN_UIDS = ['dqrHnJtM27bMpNudHxO5hL3wsNE3'];
