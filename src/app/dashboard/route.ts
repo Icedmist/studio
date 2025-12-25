7b8f9bec07afc3a414ffdd312a9c7640f8fb14fa
Iced Mist <talk2icedmist@gmail.com>
2025-12-25 10:49:05 +0000

    The path /dashboard is marked as "static" since it will be prerendered d

---
+++ src/app/dashboard/route.ts
@@ -0,0 +1,3 @@
+// This file forces the dashboard page to be rendered dynamically at request time.
+// It prevents Next.js from statically pre-rendering a page that is inherently dynamic.
+export const dynamic = 'force-dynamic';
